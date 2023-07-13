bcc6c0608679fff2bfc2e669546019d6700b45f1
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-13 18:41:28 +0530

    last commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from "./Navbar"
 
 const sidebar = () => {
   return (
-    <div>sidebar</div>
+    <div className='sidebar'>
+      <Navbar />
+    </div>
   )
 }
 
