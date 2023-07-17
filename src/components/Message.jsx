9c755082a521bd9d68d90fa182c599a75fe80076
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-17 15:58:53 +0530

    chat style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/6640696/pexels-photo-6640696.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt="chat"
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello!!</p>
+        <img
+          src="https://images.pexels.com/photos/6640696/pexels-photo-6640696.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
