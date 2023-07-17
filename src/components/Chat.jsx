130533c1fca60a1a516d65a52582277576779a3e
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-17 13:55:36 +0530

    chats changes

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Add from "../assets/add.png"
+import Option from "../assets/option.png"
+import Cam from "../assets/cam.png"
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="cam" />
+          <img src={Add} alt="add" />
+          <img src={Option} alt="option" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
