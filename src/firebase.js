ac9fa1e8a2739e151f7c50f34a1cc9a58601658a
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-17 16:31:18 +0530

    new

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs5z4HRPO6pSpyIlRgfKcjOiaYtIit2Fo",
+  authDomain: "chat-76836.firebaseapp.com",
+  projectId: "chat-76836",
+  storageBucket: "chat-76836.appspot.com",
+  messagingSenderId: "974801380316",
+  appId: "1:974801380316:web:872a61f02909f6dfb92519",
+};
+
+const app = initializeApp(firebaseConfig);
