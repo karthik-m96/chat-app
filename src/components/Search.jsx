b44e88c81147395496def6021abe673cef6d03ff
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-17 11:34:29 +0530

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a user" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/6640696/pexels-photo-6640696.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "./Navbar"
+import Search from './Search'
 
 const sidebar = () => {
   return (
     <div className='sidebar'>
       <Navbar />
+      <Search />
     </div>
   )
 }
