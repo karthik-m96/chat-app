da78406814f4204bb1a5f822a079835facb3d7e8
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-17 11:55:07 +0530

    chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/6640696/pexels-photo-6640696.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
