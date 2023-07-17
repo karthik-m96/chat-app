acbc8321931c64a9ed272f4aeaf052e67d3349a2
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-17 11:19:03 +0530

    node

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import Sidebar from "../components/Sidebar";Sidebar
 import Chat from "../components/Chat";
-=======
-import Chat from "../components/Chat";
-import Sidebar from "../components/Sidebar";
->>>>>>> 52eccf4d3b598c0006790bb2755cf936b09cfee1
-
 const Home = () => {
   return (
     <div className="home">
