104ef58600406e7e601fd1ff644a2be880a90be1
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-18 17:34:15 +0530

    new

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDs5z4HRPO6pSpyIlRgfKcjOiaYtIit2Fo",
@@ -9,4 +10,5 @@ const firebaseConfig = {
   appId: "1:974801380316:web:872a61f02909f6dfb92519",
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const Auth = getAuth();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import logo from "../assets/k1.png";
 import avatar from "../assets/gallery.png";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+  };
+
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+  
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <img src={logo} alt="logo" />
         <span className="appName">K Chat</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
           <input type="file" id="file" />
           <label htmlFor="file" className="fileLabel">
-            <img style={{width:'30px'}} src={avatar} alt="avatar" />
+            <img style={{ width: "30px" }} src={avatar} alt="avatar" />
             Add an avatar
           </label>
           <button type="button">Sign Up</button>
