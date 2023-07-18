94201495bb5626cd2ed3709d77436bc5b40e0e1e
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-18 17:56:36 +0530

    auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDs5z4HRPO6pSpyIlRgfKcjOiaYtIit2Fo",
+
   authDomain: "chat-76836.firebaseapp.com",
+
   projectId: "chat-76836",
+
   storageBucket: "chat-76836.appspot.com",
+
   messagingSenderId: "974801380316",
+
   appId: "1:974801380316:web:872a61f02909f6dfb92519",
 };
 
 export const app = initializeApp(firebaseConfig);
-export const Auth = getAuth();
+export const auth = getAuth(app);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,27 +4,24 @@ import avatar from "../assets/gallery.png";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
-
 const Register = () => {
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
   };
 
-
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
-  
   return (
     <div className="formContainer">
       <div className="formWrapper">
