a2d1d5f430c487249ca7fe3f19fb1c952caed490
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-19 16:57:21 +0530

    getting input

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs5z4HRPO6pSpyIlRgfKcjOiaYtIit2Fo",
+  authDomain: "chat-76836.firebaseapp.com",
+  projectId: "chat-76836",
+  storageBucket: "chat-76836.appspot.com",
+  messagingSenderId: "974801380316",
+  appId: "1:974801380316:web:872a61f02909f6dfb92519",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/k1.png";
 import avatar from "../assets/gallery.png";
-
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const Register = () => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName, 
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (error) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <img src={logo} alt="logo" />
         <span className="appName">K Chat</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
@@ -19,7 +53,8 @@ const Register = () => {
             <img style={{ width: "30px" }} src={avatar} alt="avatar" />
             Add an avatar
           </label>
-          <button type="button">Sign Up</button>
+          <button>Sign Up</button>
+          {err && <p className="err">Something went wrong</p>}
         </form>
         <p>You have an account? Login</p>
       </div>
